5461434d456a575a1665af3fa27bb3388aa6832d
odino <alessandro.nadalin@gmail.com>
2018-08-11 14:33:19 +0400

    kitten magic

--- xss-example/index.js
+++ xss-example/index.js
@@ -3,12 +3,22 @@ var url = require('url')
 var fs = require('fs')
 
 require('http').createServer((req, res) => {
-  res.writeHead(200, {
-    'X-XSS-Protection': 0,
-    'Content-Security-Policy': '*',
-  })
-  let search = qs.parse(url.parse(req.url).query).search
-  search = search ? `You searched for ${search}` : `Try searching...`
+  let query = qs.parse(url.parse(req.url).query)
+  let headers = {
+    'X-XSS-Protection': 0
+  }
 
-  res.end(fs.readFileSync(__dirname + '/index.html').toString().replace('__SEARCH__', search))
+  if (query.csp === 'on') {
+    headers['Content-Security-Policy'] = `default-src 'self'`
+  }
+
+  if (query.csp === 'report') {
+    headers['Content-Security-Policy-Report-Only'] = `default-src 'self'`
+  }
+
+  res.writeHead(200, headers)
+  let keyword = query.search || ''
+  let results = keyword ? `You searched for "${keyword}", we found:</br><img src="http://placekitten.com/200/300" />` : `Try searching...`
+
+  res.end(fs.readFileSync(__dirname + '/index.html').toString().replace('__KEYWORD__', keyword).replace('__RESULTS__', results))
 }).listen(7888)
